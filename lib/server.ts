f16f71da74815c1513e72e4b7bab2fc39a537717
Meno Abels <meno.abels@adviser.com>
2017-12-13 21:45:18 +0100

    * i'm crazy

--- lib/server.ts
+++ lib/server.ts
@@ -7,7 +7,7 @@ http.createServer((request, response) => {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Crazy World"
-   response.end('Hello World\n');
+   response.end('Crazy World\n');
 }).listen(8081);
 
 // Console will print the message
